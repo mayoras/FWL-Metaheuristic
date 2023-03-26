a37af2135ec40c4302b6bf1fd6cf12f5e8e3249d
Cesar Mayora <cesarmay@protonmail.com>
2023-03-26 21:50:38 +0200

    euclidean distance without weights

--- fwl/helpers.py
+++ fwl/helpers.py
@@ -1,6 +1,13 @@
+import numpy as np
+
+
 def is_float(s):
     try:
         float(s)
         return True
     except ValueError:
         return False
+
+
+def euclidean_dist(sample1: np.ndarray, sample2: np.ndarray) -> float:
+    return np.sqrt(np.sum((sample1 - sample2) ** 2))
