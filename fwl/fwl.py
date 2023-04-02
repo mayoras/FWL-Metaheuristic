212669314979545e2bc6ed10a4b180aec03eeac0
Cesar Mayora <cesarmay@protonmail.com>
2023-04-02 16:33:59 +0200

    5-fold cross validation returning pandas dataframe

--- fwl/fwl.py
+++ fwl/fwl.py
@@ -1,9 +1,22 @@
 import sys
 import numpy as np
+import pandas as pd
+import time
+
 from fwl.dataset import Dataset
-from fwl.fitness import fitness
 from fwl.knn import KNN
 from fwl.helpers import euclidean_dist
+from typing import Callable
+
+ALPHA = 0.3
+
+
+def hit_rate(num_failed: int, total: int) -> float:
+    return 100 * ((total - num_failed) / total)
+
+
+def red_rate(num_ignored: int, num_features: int) -> float:
+    return 100 * (num_ignored / num_features)
 
 
 def are_equal(e1: np.ndarray, e2: np.ndarray) -> np.bool_:
@@ -136,8 +149,91 @@ def relief_fast(x_train: np.ndarray, y_train: np.ndarray) -> np.ndarray:
     return w
 
 
-def validate(ds: Dataset):
+def validate(ds: Dataset, fwl_algo: Callable) -> pd.DataFrame:
     '''
-    5-fold cross validation
+    5-fold cross validation for a dataset
+    @param ds: Dataset used to train and validate
+    @param fwl_algo: Feature-Weight-Learning algorithm used to fit feature weights
     '''
-    return
+    clf = KNN(1)
+
+    '''
+    Measures table
+    - 4 measures: succ_rate, miss_rate, fitness, elapsed_time
+    - 5 partitions (folds) of dataset
+    '''
+    # measures = np.array(
+    #     [np.zeros(4), np.zeros(4), np.zeros(4), np.zeros(4), np.zeros(4)],
+    #     dtype=np.float32,
+    # )
+    measures = np.array(np.repeat(0, 6 * 4), dtype=np.float32).reshape((6, 4))
+
+    for test_part_key in range(1, 6):
+        # Training stage
+        x_train = np.concatenate(
+            [
+                ds.partitions[i]
+                for i in filter(lambda x: x != test_part_key, ds.partitions)
+            ]
+        )
+        y_train = np.concatenate(
+            [ds.classes[i] for i in filter(lambda x: x != test_part_key, ds.partitions)]
+        )
+
+        start = time.time()
+        w = fwl_algo(x_train=x_train, y_train=y_train)
+        end = time.time()
+
+        # Testing stage
+        test_part = ds.partitions[test_part_key]
+        test_y = ds.classes[test_part_key]
+
+        # Fit the model
+        clf.fit(X=x_train, y=y_train, w=w)
+
+        # Get predictions with  weights
+        predictions = clf.predict(test_part)
+
+        # Test predictions
+        num_failed = 0
+        for _inp, prediction, label in zip(test_part, predictions, test_y):
+            if prediction != label:
+                # print(
+                #     inp, 'has been classified as ', prediction, 'and should be ', label
+                # )
+                num_failed += 1
+        # num_success = len(test_part) - num_failed
+
+        num_feats_ignored = len(w[w < 0.1])
+
+        # Calc measurements
+        hit_r = hit_rate(num_failed=num_failed, total=len(test_part))
+        reduction_rate = red_rate(
+            num_ignored=num_feats_ignored, num_features=test_part.shape[1]
+        )
+        fitness = ALPHA * hit_r + (1 - ALPHA) * reduction_rate
+        fwl_elapsed_time = end - start
+
+        # print(f'Fitness({[num for num in w]}) = {fitness}')
+
+        measures[test_part_key - 1] = np.array(
+            [hit_r, reduction_rate, fitness, fwl_elapsed_time]
+        )
+
+    # Calc mean of each statistic
+    measures[-1] = np.sum(measures[:-1], axis=0) / (measures.shape[0] - 1)
+
+    # return the pandas DataFrame
+    rows = np.array(
+        [
+            'Partición 1',
+            'Partición 2',
+            'Partición 3',
+            'Partición 4',
+            'Partición 5',
+            'Media',
+        ]
+    )
+    cols = np.array(['%_clas', '%_red', 'Fit.', 'T'])
+    df = pd.DataFrame(measures, index=rows, columns=cols)
+    return df
