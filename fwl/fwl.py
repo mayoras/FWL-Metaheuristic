0ef9c686f0830f26872b870965371490adaf95d2
Cesar Mayora <cesarmay@protonmail.com>
2023-04-02 03:34:57 +0200

    RELIEF FWL algorithm v2.0 super fast

--- fwl/fwl.py
+++ fwl/fwl.py
@@ -67,6 +67,75 @@ def relief(x_train: np.ndarray, y_train: np.ndarray) -> np.ndarray:
     return w
 
 
+def relief_fast(x_train: np.ndarray, y_train: np.ndarray) -> np.ndarray:
+    '''
+    @brief learn weights from a training set using a Greedy approach
+    @param x_train training examples
+    @param y_train training classes
+    @return w weights learned
+    '''
+
+    # Initialize W to 0
+    w = np.zeros(x_train.shape[1], dtype=np.float32)
+
+    # Precalculate all pair-wise distances:
+    # Res: https://sparrow.dev/pairwise-distance-in-numpy/
+    dists = np.linalg.norm(
+        x_train[:, np.newaxis, :] - x_train[np.newaxis, :, :], axis=-1
+    )
+
+    ####### TODO: Maybe do this a little bit faster, maybe with new axes or something like that
+    # Get all nearest friends and enemies
+    friends = []
+    enemies = []
+    for i in range(x_train.shape[0]):
+        friend = None
+        enemy = None
+        for j in range(dists[i].shape[0]):
+            # Look for nearest friend for ith example
+            min_dist_f = sys.maxsize
+            min_dist_e = sys.maxsize
+
+            if (
+                dists[i, j] > 0
+                and dists[i, j] < min_dist_f
+                and y_train[i] == y_train[j]
+            ):
+                min_dist_f = dists[i, j]
+                friend = x_train[j]
+            if (
+                dists[i, j] > 0
+                and dists[i, j] < min_dist_e
+                and y_train[i] != y_train[j]
+            ):
+                min_dist_e = dists[i, j]
+                enemy = x_train[j]
+        friends.append(friend)
+        enemies.append(enemy)
+    friends = np.array(friends)
+    enemies = np.array(enemies)
+
+    # For each sample in the training set
+    for i in range(x_train.shape[0]):
+        # Look for the nearest enemy and friend
+        enemy = enemies[i]
+        friend = friends[i]
+
+        # Update the weights with the component-wise distances from enemy and friend
+        w = w + np.abs(x_train[i] - enemy) - np.abs(x_train[i] - friend)
+
+    # Normalize weights
+    w_max = np.max(w)
+
+    for idx, w_i in enumerate(w):
+        if w_i < 0:
+            w[idx] = 0
+        else:
+            w[idx] = w_i / w_max
+
+    return w
+
+
 def validate(ds: Dataset):
     '''
     5-fold cross validation
