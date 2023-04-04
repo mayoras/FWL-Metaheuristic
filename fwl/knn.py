3d28532872ea6e9acfdca6e4a1bd5919613fb1a0
Cesar Mayora <cesarmay@protonmail.com>
2023-04-04 02:07:55 +0200

    k-nn faster predictions

--- fwl/knn.py
+++ fwl/knn.py
@@ -1,5 +1,5 @@
 import numpy as np
-from fwl.helpers import euclidean_dist, most_common, are_equal
+from fwl.helpers import euclidean_dist, most_common
 
 
 class KNN:
@@ -11,17 +11,31 @@ class KNN:
         self.y_train = y
         self.w_train = w
 
-    def predict(self, samples: np.ndarray) -> np.ndarray:
+    # def predict(self, samples: np.ndarray) -> np.ndarray:
+    #     classes: list[float] = []
+    #     for s in samples:
+    #         # get the distances from each training sample to the unclassified sample
+    #         distances = []
+    #         for x in self.X_train:
+    #             # Leave-one-out
+    #             if not are_equal(x, s):
+    #                 distances.append(euclidean_dist(x, s, self.w_train))
+
+    #         # get k-nearest neighbours
+    #         k_idx = np.argsort(distances)[: self.k]
+
+    #         # if k > 1, the class assigned to the example is the mode of the k classes
+    #         classes.append(most_common(self.y_train[k_idx]))
+    #     return np.array(classes, dtype=np.float32)
+
+    def predict(self, examples: np.ndarray) -> np.ndarray:
         classes: list[float] = []
-        for s in samples:
-            # get the distances of each training sample to the unclassified sample
-            distances = []
-            for x in self.X_train:
-                # Leave-one-out
-                if not are_equal(x, s):
-                    distances.append(euclidean_dist(x, s, self.w_train))
+        for e in examples:
+            distances = euclidean_dist(self.X_train, e, self.w_train)
 
-            k_idx = np.argsort(distances)[: self.k]
+            # get k-nearest neighbours. Leave-one-out by skipping the first distance (that is the example itself)
+            k_idx = np.argsort(distances)[1 : self.k + 1]
 
+            # if k > 1, the class assigned to the example is the mode of the k classes
             classes.append(most_common(self.y_train[k_idx]))
         return np.array(classes, dtype=np.float32)
