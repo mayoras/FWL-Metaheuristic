af07e563fe54f6a232bbf23770f9d66226fdd683
Cesar Mayora <cesarmay@protonmail.com>
2023-04-07 15:40:19 +0200

    fix: fixed bug leave-one-out on predicting same dataset

--- fwl/knn.py
+++ fwl/knn.py
@@ -30,11 +30,19 @@ class KNN:
 
     def predict(self, examples: np.ndarray) -> np.ndarray:
         classes: list[float] = []
+
+        predicting_training = False
+        # Check if we are predicting in training - Leave-one-out
+        if examples.shape == self.X_train.shape and np.allclose(examples, self.X_train):
+            predicting_training = True
         for e in examples:
             distances = euclidean_dist(self.X_train, e, self.w_train)
 
-            # get k-nearest neighbours. Leave-one-out by skipping the first distance (that is the example itself)
-            k_idx = np.argsort(distances)[1 : self.k + 1]
+            # get k-nearest neighbours. Leave-one-out (in training) by filtering distances greater than 0 (that is the example itself)
+            if predicting_training:
+                k_idx = np.argsort(distances)[1 : self.k + 1]
+            else:
+                k_idx = np.argsort(distances)[: self.k]
 
             # if k > 1, the class assigned to the example is the mode of the k classes
             classes.append(most_common(self.y_train[k_idx]))
