4aa69ef724d7c6494cf458f1ddcc6bed35089480
Cesar Mayora <cesarmay@protonmail.com>
2023-04-09 02:08:46 +0200

    use one unique seed

--- main.py
+++ main.py
@@ -2,8 +2,6 @@ import argparse
 from fwl.dataset import Dataset
 import fwl.fwl as fwl
 
-# def greedy(ds: Dataset) -> tuple[np.ndarray, float]:
-
 ALGORITHMS = {
     'one-kk': fwl.one_kk,
     'greedy': fwl.greedy,
@@ -11,16 +9,16 @@ ALGORITHMS = {
 }
 
 
-def score(dataset, alg, seeds):
+def score(dataset, alg, seed):
     if len(alg) == 1:
         print(alg[0].upper().replace('-', ' '))
-        score = fwl.validate(ds=dataset, fwl_algo=ALGORITHMS[alg[0]], seeds=seeds)
+        score = fwl.validate(ds=dataset, fwl_algo=ALGORITHMS[alg[0]], seed=seed)
         print(score)
     else:
         for a in ALGORITHMS:
             print(a.upper().replace('-', ' '))
-            score = fwl.validate(ds=dataset, fwl_algo=ALGORITHMS[a], seeds=seeds)
-            print(score + '\n')
+            score = fwl.validate(ds=dataset, fwl_algo=ALGORITHMS[a], seed=seed)
+            print(score, '\n')
 
 
 def main():
@@ -30,30 +28,14 @@ def main():
         '--algorithm', type=str, default=False, help='choose the algorithm'
     )
     parser.add_argument(
-        '--seeds',
-        type=str,
-        default=False,
-        help='Seeds for the pseudorandom generator in each execution',
+        '--seed',
+        type=int,
+        default=True,
+        help='Seed for the pseudorandom generator',
     )
     parser.add_argument('--list-algo', default=False, action='store_true')
 
     args = parser.parse_args()
-
-    # Seed
-    if args.seeds:
-        seed_list = args.seeds.split(',')
-        # General seed
-        if len(seed_list) == 1:
-            seeds = [seed_list[0] for _ in range(5)]
-        # Invalid number of seeds
-        elif len(seed_list) != 5:
-            print(
-                'Invalid number of seeds.\nIntroduce at least 5 seeds OR specify just one general seed.'
-            )
-            exit(1)
-        seeds = [int(seed) for seed in args.seeds.split(',')]
-    else:
-        seeds = [0 for _ in range(5)]
 
     # List available algorithms
     if args.list_algo:
@@ -61,6 +43,12 @@ def main():
         for a in ALGORITHMS:
             print('->', a)
         return
+
+    # Seed
+    if args.seed:
+        seed = int(args.seed)
+    else:
+        seed = 0
 
     # Dataset
     if args.dataset:
@@ -84,11 +72,11 @@ def main():
 
     if len(data) == 1:
         print(f'-------------- {data[0].ds_name} --------------')
-        score(data[0], alg, seeds)
+        score(data[0], alg, seed)
     else:
         for d in data:
             print(f'-------------- {d.ds_name} --------------')
-            score(d, alg, seeds)
+            score(d, alg, seed)
 
 
 if __name__ == '__main__':
